85790dedc19162dbdf9b3e64688acbc2fbb550f7
Geoffroy Weyer <webweyer@gmail.com>
2016-03-10 15:17:20 +0100

    eureka client

--- src/main/java/io/yac/rates/Application.java
+++ src/main/java/io/yac/rates/Application.java
@@ -2,11 +2,13 @@ package io.yac.rates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by geoffroy on 17/02/2016.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class Application {
 
     public static void main(String[] args) {
